a49dbf9c3e61dfb0c9b2aad9515d7fcd37c49bd7
amberperrym <amber.perry-marinov@mail.mcgill.ca>
2023-04-20 22:20:30 -0400

    Home Page updates

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 <footer>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-6 text-left text-center mb-3 mb-md-0">
-        <p style="color:#EDD5D0;">© The Leveret | All Rights Reserved</p>
+      <div class="col-md-4 text-center d-flex align-items-center justify-content-center mb-3 mb-md-0">
+        <p style="color:#2D3334;">© The Leveret | All Rights Reserved</p>
       </div>
-      <div class="col-md-6 text-right text-center">
+      <div class="col-md-4 text-center d-flex align-items-center justify-content-center">
+        <p style="color:#2D3334;">Email us at info@theleveret.com</p>
+      </div>
+      <div class="col-md-4 text-center d-flex align-items-center justify-content-center">
         <!-- Add icon library -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Add font awesome icons -->
--- index.php
+++ index.php
@@ -7,102 +7,108 @@
         <title>The Leveret Homepage</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro&family=Crimson+Text&family=Merriweather&family=Merriweather+Sans&family=Scheherazade+New&display=swap" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body class="p-3 m-0 border-0 bd-example">
 <?php include('header.php'); ?><br>
 
-<div class="container-fluid bg-3 text-center">    
-  <h3>Recent Articles</h3><br>
-</div>
+<div class="article-container">
+  <div class="bg-3 text-center">   
+    <h3>Recent Articles</h3><br>
+  </div>
 
-<! -- first row of articles-->
-<div class="container-fluid">
-  <div class="row">
+  <! -- first row of articles-->
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="col-sm-3">
+    <div style="position: relative; width: 100%;">
+      <img width="100%" height="175" src="assets/img/FionaBenson.jpeg" style="object-fit: cover;">
+      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+        <h5>Article Title</h5>
+        <p>Article summary or description</p>
+      </div>
+    </div>
+  </div>
+    <div class="col-sm-3">
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/CharlesDarwin.webp" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
+      </div>
+    </div>
 
     <div class="col-sm-3">
-  <div style="position: relative; width: 100%;">
-    <img width="100%" height="175" src="assets/img/FionaBenson.jpeg" style="object-fit: cover;">
-    <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-      <h5>Article Title</h5>
-      <p>Article summary or description</p>
-    </div>
-  </div>
-</div>
-  <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/CharlesDarwin.webp" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/JohnJamesA.jpeg" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/JohnJamesA.jpeg" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/HistoryOfScience.jpeg" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div><br>
-
-  <! -- second row of articles -->
-<div class="container-fluid">    
-  <div class="row">
     <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/Marg.Cavendish.jpeg" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/HistoryOfScience.jpeg" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
       </div>
     </div>
-  </div>
-      
-  <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/Walden.jpeg" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
-      </div>
     </div>
-  </div>
+    </div><br>
 
-  <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/Hieroglyphs2.jpeg" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
+    <! -- second row of articles -->
+  <div class="container-fluid">    
+    <div class="row">
+      <div class="col-sm-3">
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/Marg.Cavendish.jpeg" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
       </div>
     </div>
-  </div>
+        
+    <div class="col-sm-3">
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/Walden.jpeg" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
+      </div>
+    </div>
 
-  <div class="col-sm-3">
-    <div style="position: relative; width: 100%;">
-      <img width="100%" height="175" src="assets/img/AnneCarson.webp" style="object-fit: cover;">
-      <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
-        <h5>Article Title</h5>
-        <p>Article summary or description</p>
+    <div class="col-sm-3">
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/Hieroglyphs2.jpeg" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-3">
+      <div style="position: relative; width: 100%;">
+        <img width="100%" height="175" src="assets/img/AnneCarson.webp" style="object-fit: cover;">
+        <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 10px;">
+          <h5>Article Title</h5>
+          <p>Article summary or description</p>
+        </div>
       </div>
     </div>
   </div>
+</div><br>
 </div><br>
 
 <button type="button" class="block">Subscribe to our Newsletter</button><br>
