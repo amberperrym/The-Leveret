794e144e6d1baae785cd20290ac2f3fddd234934
CamilleGronan <130411154+CamilleGronan@users.noreply.github.com>
2023-04-22 23:09:22 -0400

    Add files via upload
    Created pages "about", "history", "anthropology" and "literature".  Only "about" has content, others are just shells to allow all pages to be active in navigation.  Link social media to icons.  Directory /wamp64/www/leveret should be created for WAMP to run blog.

---
+++ about.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<link rel="stylesheet" href="assets/css/MyStyle.css">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>People Behind the Leveret</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro&family=Crimson+Text&family=Merriweather&family=Merriweather+Sans&family=Scheherazade+New&display=swap" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body class="p-3 m-0 border-0 bd-example">
+<?php include('header.php'); ?><br>
+
+<div class="bg-3 text-center">   
+    <h3>Our Contributors</h3><br>
+    <hr>
+</div>
+<div class="container-fluid !align !spacing">
+
+</div>
+ <div class="row">
+  <div class="col-sm-4 ">
+      
+        <img src="/leveret/assets/img/camille.jpg" class="img-fluid" alt="image desc">
+        
+  </div>
+    <div class="col-sm-4 ">
+      
+          <p>"Camille Gronan, a lifelong learner and animal lover. I graduated with honors from Dawson College in liberal arts and then pursued my interests in anthropology, history, and religious studies at McGill University, where I earned a BA degree. Currently, I am completing a Master's in Information Technology at McGill. When I'm not studying, you can find me taking care of my beloved menagerie of fish, amphibians, a bunny, and a dove. I am passionate about exploring new ideas and technologies while also nurturing my animal companions."
+ </p>
+  </div>
+  <hr>
+</div>
+  <div class="row">
+  <div class="col-sm-4">
+      
+        <img src="/leveret/assets/img/camille.jpg" class="img-fluid" alt="image desc">
+        
+  </div>
+    <div class="col-sm-4">
+      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida. Venenatis lectus magna fringilla urna. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. In nulla posuere sollicitudin aliquam ultrices. Sit amet volutpat consequat mauris nunc congue nisi. Sit amet nisl purus in. Tincidunt arcu non sodales neque sodales ut etiam sit. Eget gravida cum sociis natoque penatibus. Id aliquet lectus proin nibh nisl condimentum id venenatis a.
+      </p>
+  </div>
+</div>
+</div>
+<hr>
+
+<div class="bg-3 text-center">   
+    <h3>How To Cite Our Blog</h3><br>
+</div>
+
+  <div class="container-fluid !direction !spacing">
+    <div class="row>
+      <div class="col-sm-2">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida. Venenatis lectus magna fringilla urna. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. In nulla posuere sollicitudin aliquam ultrices. Sit amet volutpat consequat mauris nunc congue nisi. Sit amet nisl purus in. Tincidunt arcu non sodales neque sodales ut etiam sit. Eget gravida cum sociis natoque penatibus. Id aliquet lectus proin nibh nisl condimentum id venenatis a.
+      </p>
+      </div>
+    </div>  </div>
+<button type="button" class="block">Subscribe to our Newsletter</button><br>
+
+<?php include('footer.php'); ?>
+</body>
+</html>
